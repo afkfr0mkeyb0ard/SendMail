a6f075c2cc818428643653d37abe0b130835fa28
afkfr0mkeyb0ard <123593001+afkfr0mkeyb0ard@users.noreply.github.com>
2024-10-08 12:53:15 +0200

    Update sendmail.py

--- sendmail.py
+++ sendmail.py
@@ -1,14 +1,71 @@
 from email.mime.text import MIMEText
 import smtplib
 from email.message import EmailMessage
+import sys
+
 msg = EmailMessage()
-email_body = """<p>Hello,</p><p>We just created a new offer for you!!</p><p><b>90% discount on all our products</b></p><p>Please check the following link for more information:</p><a href='https://thisisafakeurl.com'>Click here!</a>
-<p>Enjoy!</p></br><p>This is a fake email for testing.</p>"""
+
+if not "-to" in sys.argv:
+	sys.exit("[-] Please specify an address where to send the mail (-to recipient@domain.com).")
+else:
+	i = sys.argv.index("-to")
+	try:
+		EMAIL_TO = sys.argv[i+1]
+	except:
+		sys.exit("[-] The recipient address is invalid (-to recipient@domain.com).")
+		
+	
+if not "-from" in sys.argv:
+	sys.exit("[-] Please specify a sender address (-from sender@domain.com).")
+else:
+	i = sys.argv.index("-from")
+	try:
+		EMAIL_FROM = sys.argv[i+1]
+	except:
+		sys.exit("[-] The sender address is invalid (-from sender@domain.com).")
+
+if not "-t" in sys.argv:
+	sys.exit("[-] Please specify a template file (-t newoffer.html).")
+else:
+	i = sys.argv.index("-t")
+	try:
+		TEMPLATE = sys.argv[i+1]
+	except Exception as e:
+		sys.exit("[-] The template file is invalid or does not exist in templates folder (-t newoffer.html).")
+
+if not "-s" in sys.argv:
+	sys.exit("[-] Please specify a subject (-s 'New offer!!').")
+else:
+	i = sys.argv.index("-s")
+	try:
+		SUBJECT = sys.argv[i+1]
+	except Exception as e:
+		sys.exit("[-] The subject is invalid (-s 'New offer!!').")
+
+if "-rt" in sys.argv:
+	i = sys.argv.index("-rt")
+	try:
+		REPLY_TO = sys.argv[i+1]
+	except Exception as e:
+		sys.exit("[-] The reply-to address is invalid (-rt attacker@domain.com).")
+
+
+with open('templates/'+TEMPLATE, 'r') as f:
+    email_body = f.read()
+
 msg = MIMEText(email_body,'html')
-msg["Subject"] = "New offer for you!!"
-msg['X-MSMail-Priority'] = 'High'
-msg["To"] = "<VICTIM_EMAIL>"
-msg["From"] = "<SPOOFED_EMAIL>"
-msg.add_header("reply-to","<ATTACKER_EMAIL>")
+msg["Subject"] = SUBJECT
+print("[+] Subject: " + SUBJECT)
+if "-H" in sys.argv:
+	msg['X-MSMail-Priority'] = 'High'
+	print("[+] Set email priority to High")
+msg["From"] = EMAIL_FROM
+print("[+] Sender: " + EMAIL_FROM)
+msg["To"] = EMAIL_TO
+print("[+] Sending to: " + EMAIL_TO)
+if "-rt" in sys.argv:
+	msg.add_header("reply-to",REPLY_TO)
+	print("[+] Set reply-to: " + REPLY_TO)
 s = smtplib.SMTP("localhost")
 s.send_message(msg)
+print("[+] Email sent successfully!")
